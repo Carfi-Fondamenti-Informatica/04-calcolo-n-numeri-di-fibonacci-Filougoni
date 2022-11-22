740d129b2e2ef7644bec461ff5604087519c4273
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-11-22 16:50:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n, a=1,b=1,c;
+       cin >> n;
+       do{
+           cout << a << endl;
+           cout<< b << endl;
+       }
+       while(n<3);
+           for (int i = 2; i < n; i++){
+               c = a + b;
+               a = b;
+               b = c;
+               cout << c << endl;
+           }
    return 0;
 }
