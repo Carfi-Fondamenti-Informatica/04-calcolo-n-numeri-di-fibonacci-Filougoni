b4a05af48d1815a89d2b05e03c403d0cc2d340df
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-11-24 20:44:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int n, a=1,b=1,c;
-       cin >> n;
-       do{
-           cout << a << endl;
-           cout<< b << endl;
+int n=0,a=1,b=1,c=1;
+       cin>>n;
+       if(n>=2){
+           cout << a << endl<< b << endl;
+       for (int i = 2; i < n; i++) {
+           c=a+b;
+           a=b;
+           b=c;
+           cout << c << endl;
        }
-       while(n<3);
-           for (int i = 2; i < n; i++){
-               c = a + b;
-               a = b;
-               b = c;
-               cout << c << endl;
-           }
+       }
+       else{
+           cout << "errore"<<endl;
+
+       }
    return 0;
 }
